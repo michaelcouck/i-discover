f73b11d93edb6364e5706db1b73ce9023f0ad2af
michaelcouck <Caherl1ne>
2016-01-03 18:35:42 +0100

    Added Neuroph for the deep learning API.

--- src/test/java/ikube/discover/analyze/train/NeurophTrainTest.java
+++ src/test/java/ikube/discover/analyze/train/NeurophTrainTest.java
@@ -14,6 +14,7 @@ public class NeurophTrainTest extends AbstractTest {
     @Test
     public void train() throws IOException {
         neurophTrain.train();
+        neurophTrain.trainMinst();
     }
 
 }
